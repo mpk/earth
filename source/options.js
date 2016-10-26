a72ad2348edb3fe0cde59aa22e81fc246533e10e
mpk <mpk@users.noreply.github.com>
2016-10-26 22:25:02 +0200

    Added Q3 2016 + updated Q2 2016 data

--- source/options.js
+++ source/options.js
@@ -6,7 +6,7 @@ import TimeRange from './utils/time-range';
 export default {
 
 	initTimeRange: new TimeRange(1404172800000, 1435708799000),
-	maxTimeRange: new TimeRange(1262304000000, 1467331199000),
+	maxTimeRange: new TimeRange(1262304000000, 1475279999000),
 
 	animationSpeed: 5000000,
 
